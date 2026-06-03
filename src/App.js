1b1c503190af69e576adbbafc5a6d4bcda81ebc2
Matheus Stopinski <matheus.stopinski@hotmail.com>
2026-06-03 11:52:33 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
+/*
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to My React App</h1>
-      <p>This is a simple React application.</p>
+      <Titulo />
+    </div>
+  );
+}
+
+function Titulo() {
+  return (
+    <div className="header">
+      <h1>Welcome to My React App!</h1>
+      <p>This is a simple React application</p>
       <h2>npm start</h2>
+    </div>
+  );
+}
+
+export default App;
+
+*/
+
+// [ Modo propriedades! ]
+
+/*
+
+import './App.css';
+
+function Header(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <p>{props.subtitle}</p>
+      <h2>{props.tagline}</h2>
+    </>
+  );
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header 
+
+        title="Welcome to My React App!"
+        subtitle="This is a simple React application"
+        tagline="npm start"
+
+      />
+
+    </div>
+  );
+}
+
+  
+export default App;
+
+*/
+
+
+import './App.css';
+
+function Header(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <p>{props.subtitle}</p>
+      <h2 className={props.taglineClass}>
+        {props.tagline}
+      </h2>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header
+        title="Welcome to My React App!"
+        subtitle="This is a simple React application"
+        tagline="npm start"
+        taglineClass="tagline"
+      />
     </div>
   );
 }
