f5ae295a8c7f757a338b1bd7e7cae1d16c79fd32
Matheus Stopinski <matheus.stopinski@hotmail.com>
2026-06-05 10:56:34 -0300

    components and properties

--- src/App.js
+++ src/App.js
@@ -33,45 +33,11 @@ import './App.css';
 function Header(props) {
   return (
     <>
+
       <h1>{props.title}</h1>
       <p>{props.subtitle}</p>
-      <h2>{props.tagline}</h2>
-    </>
-  );
-}
+      <h2 className={props.taglineClass}>{props.tagline}</h2>
 
-
-function App() {
-  return (
-    <div className="App">
-      <Header 
-
-        title="Welcome to My React App!"
-        subtitle="This is a simple React application"
-        tagline="npm start"
-
-      />
-
-    </div>
-  );
-}
-
-  
-export default App;
-
-*/
-
-
-import './App.css';
-
-function Header(props) {
-  return (
-    <>
-      <h1>{props.title}</h1>
-      <p>{props.subtitle}</p>
-      <h2 className={props.taglineClass}>
-        {props.tagline}
-      </h2>
     </>
   );
 }
@@ -80,6 +46,38 @@ function App() {
   return (
     <div className="App">
       <Header
+
+        title="Welcome to My React App!"
+        subtitle="This is a simple React application"
+        tagline="npm start"
+        taglineClass="tagline"
+
+      />
+    </div>
+  );
+}
+
+export default App;
+
+*/
+
+// [ Modo propriedades! - Desestruturação! ]
+
+import './App.css';
+
+function Header({ title, subtitle, tagline, taglineClass }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      <h2 className={taglineClass}>{tagline}</h2>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App"><Header
         title="Welcome to My React App!"
         subtitle="This is a simple React application"
         tagline="npm start"
@@ -90,3 +88,4 @@ function App() {
 }
 
 export default App;
+
