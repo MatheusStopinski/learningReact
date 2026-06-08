f3a8e52bdde1cb62e2236f05577ffa20ebda3057
Matheus Stopinski <matheus.stopinski@hotmail.com>
2026-06-08 18:28:07 -0300

    shared state management

--- src/compenents/counter.js
+++ src/compenents/counter.js
@@ -1,22 +1,57 @@
-import { useState } from 'react';
+import { createContext, useState, useContext } from 'react';
+
+const CounterContext = createContext();
 
 
-export function Counter() {
-    const [counter, setCounter] = useState(0);
+function CounterContextProvider({ children }) {
+        const [counter, setCounter] = useState(0);
+        const [CounterHistory, setCounterHistory] = useState([]);
 
-    return <div>  
-        <CounterArea counter={counter} />
-         &nbsp;
-         &nbsp;
-        <CounterIncrementButton setCounter={setCounter} />
-    </div>
+        return (<CounterContext.Provider value={{ 
+            counter, setCounter, CounterHistory, setCounterHistory 
+        }}>
+            {children}
+        </CounterContext.Provider>); 
+    }
+
+
+export function Counter() {    
+    return <CounterContextProvider>  
+        <CounterArea />
+        <CounterHistoryList />
+        <CounterIncrementButton />
+        </CounterContextProvider>
 };
 
-function CounterArea({ counter }) {    
+function CounterArea() {   
+    const { counter } = useContext(CounterContext);
     return <span>{counter}</span>   
 };  
 
-function CounterIncrementButton({ setCounter }) {
-    return <button onClick={() => setCounter(prev => prev + 1)}>+1</button>
+function CounterIncrementButton() {
+    const { setCounter, 
+            counter, 
+            setCounterHistory, 
+            CounterHistory 
+        } = useContext(CounterContext);
+
+    function increment() {
+        setCounterHistory([...CounterHistory, counter]); //destrutor de propriedades.
+        setCounter(prev => prev + 1);
+    }
+
+    return <button onClick={increment}>
+        Counter
+        </button>
 };  
 
+function CounterHistoryList() {
+    const { CounterHistory } = useContext(CounterContext);
+    return (
+        <ul>
+            {CounterHistory.map(i => <ol key={i}>
+                 {i}
+            </ol>)}
+        </ul>
+    );
+}
